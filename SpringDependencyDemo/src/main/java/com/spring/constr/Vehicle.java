205fec3b46fe0c549930760f3a0a90d8f0934984
shristitechacademy <sripriyamathan@gmail.com>
2024-12-19 18:29:13 +0530

    dependency injection

---
+++ SpringDependencyDemo/src/main/java/com/spring/constr/Vehicle.java
@@ -0,0 +1,39 @@
+package com.spring.constr;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Vehicle {
+
+	private String brand;
+	private String model;
+	private Insurance insurance;
+	private Car car;
+	// no need of autowired
+	// no default constructor allowed
+	public Vehicle(Insurance insurance) {
+		super();
+		this.insurance = insurance;
+	}
+	@Value("${vehicle.brand}")
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	@Value("${vehicle.model}")
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+
+	public void showInsurance() {
+		System.out.println("Printing Insurance for vehicle "+model);
+		System.out.println(insurance.getType());
+	}
+	
+	
+}
+
+
+
+
